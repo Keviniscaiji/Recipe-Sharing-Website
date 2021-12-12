b744d5ebc8184b17e3ee508b29785609e7287ff3
KaiwenGong <kaiwen.gong@ucdconnect.ie>
2021-12-12 19:59:28 +0800

    update dishmicro

--- dishmicro/dishapp/__init__.py
+++ dishmicro/dishapp/__init__.py
@@ -14,33 +14,21 @@ from dishapp import routes, models
 
 # import logging
 app.logger = logging.getLogger()
-# app.logger.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s-%(levelname)s: %(message)s')
-# console_handler = logging.StreamHandler(sys.stdout)
-# console_handler.formatter = formatter
-# console_handler.setLevel(logging.DEBUG)
-# app.logger.addHandler(console_handler)
 
-# logfile = 'DEBUG.log'
-# File_handler = logging.FileHandler(logfile, mode='a', encoding='utf8')
-# File_handler.setFormatter(formatter)
-# File_handler.setLevel(logging.DEBUG)
-# app.logger.addHandler(File_handler)
-# handler1 = logging.StreamHandler(sys.stdout)
-
-handler1 = logging.FileHandler("logs/Error.log",mode="w")
+# add the handler for Error and create a log file
+handler1 = logging.FileHandler("logs/Error.log",mode="a")
 formatter = logging.Formatter('%(asctime)s %(name)-s %(levelname)-s %(message)s')
 handler1.setFormatter(formatter)
 handler1.setLevel(logging.ERROR)
 app.logger.addHandler(handler1)
-#
-handler2 = logging.FileHandler("logs/Debug.log",mode="w")
+# add the handler for Debug and create a log file
+handler2 = logging.FileHandler("logs/Debug.log",mode="a")
 formatter = logging.Formatter('%(asctime)s %(name)-s %(levelname)-s %(message)s')
 handler2.setFormatter(formatter)
 handler2.setLevel(logging.DEBUG)
 app.logger.addHandler(handler2)
-#
-handler3 = logging.FileHandler("logs/Info.log",mode="w")
+# add the handler for Info and create a log file
+handler3 = logging.FileHandler("logs/Info.log",mode="a")
 formatter = logging.Formatter('%(asctime)s %(name)-s %(levelname)-s %(message)s')
 handler3.setFormatter(formatter)
 handler3.setLevel(logging.INFO)
